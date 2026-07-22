f0ef02797fca32929ce1a07cc2a24e8ac2e73744
nguyen.van.su <nguyen.van.su@quantic.com.vn>
2026-07-22 09:27:32 +0700

    add karaoke

--- README.md
+++
@@ -1,2 +0,0 @@
-# vianne
-for Zizi
--- asset-manifest.js
+++
@@ -1,7 +0,0 @@
-// asset-manifest.js
-let ASSET_MANIFEST = [
-    '/',
-    'styles.css',
-    'assets/icons/icon-192x192.png',
-    'assets/icons/icon-512x512.png'
-  ];
